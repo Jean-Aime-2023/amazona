0aabebd3c8dbaf931d790fb9acb53a957118e01c
Jean-Aime-2023 <124204227+Jean-Aime-2023@users.noreply.github.com>
2023-10-20 16:01:09 +0200

    Complete add to cart functionality

--- backend/server.js
+++ backend/server.js
@@ -8,13 +8,22 @@ app.get('/api/products', (req, res) => {
 });
 
 app.get('/api/products/slug/:slug', (req, res) => {
-    const product= data.products.find(x => x.slug === req.params.slug);
-    if(product){
-        res.send(product)
-    }else{
-        res.send(404).send({message: 'Product Not Found'})
-    }
-  });
+  const product = data.products.find((x) => x.slug === req.params.slug);
+  if (product) {
+    res.send(product);
+  } else {
+    res.send(404).send({ message: 'Product Not Found' });
+  }
+});
+
+app.get('/api/products/:id', (req, res) => {
+  const product = data.products.find((x) => x._id === req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.send(404).send({ message: 'Product Not Found' });
+  }
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
